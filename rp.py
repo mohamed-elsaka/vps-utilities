91e4115cc7c9bafeb44d2389ec783dcc0a6b6d65
mohamed-elsaka <islamona2008@gmail.com>
2015-09-05 07:41:19 +0200

    bug: global var

--- rp.py
+++ rp.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
-# This script batch renames multiple files in current directory
+# This script batch renames multiple files in a directory
 # Mainly for 
 from os import rename, listdir
 import sys
 
+global renameDir 
 renameDir = "/var/www/rl/files/"
 
 def renameFiles(stringToBeReplaced, stringToReplaceOld, isPreview):
-    
-    if( ! renameDir.endswith("/") ):
-        renameDir += "/"
+
+    global renameDir
+
+    if( not renameDir.endswith('/') ):
+    renameDir += '/'
 
     fnames = listdir(renameDir)
 
@@ -18,22 +21,27 @@ def renameFiles(stringToBeReplaced, stringToReplaceOld, isPreview):
             newName = fname.replace(stringToBeReplaced, stringToReplaceOld, 1)
             if( isPreview == False ): 
                 rename( renameDir + fname, renameDir + newName)
-                print renameDir+fname
+                #print renameDir+fname
             else:
                 print "  '" + fname+"' => '"+newName+"'"
 
 if( len(sys.argv) < 3 ):
-    print "This script batch renames multiple files in current directory"
-    print "Please pass the args. stringToBeReplaced stringToReplaceOld"
+    print "This script batch renames multiple files in a directory"
+    print "Please pass the args. stringToBeReplaced stringToReplaceOld  [renameDir='/var/www/rl/files']"
     print "ex 1: python rp.py \"--\" \"-\" "
     print "      converts \"name1--name2\" to \"name1-name2\" "
+    print "ex 2: python rp.py \"--\" \"-\" \"/var/www/rl/files/\" "
+    print "      converts \"name1--name2\" to \"name1-name2\" in directory \"/var/www/rl/files/\" "
+
 
 elif(len(sys.argv) >= 3):
     stringToBeReplaced = sys.argv[1]
     stringToReplaceOld = sys.argv[2]
 
     if(len(sys.argv) == 4):
-        renameDir = sys.argv[3]
+    print "args = 4"
+        #global renameDir 
+    renameDir = sys.argv[3]
     
     print "Will be renaming these files:"
     renameFiles(stringToBeReplaced, stringToReplaceOld, True ) #isPreview = True
