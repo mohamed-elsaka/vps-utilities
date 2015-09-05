b7907d714f982b4f0672529d581d73ac1fb0ec68
mohamed-elsaka <islamona2008@gmail.com>
2015-09-05 05:47:55 +0200

    fixed bug: checking if file exists before uploading

--- youtube-batch_up.sh
+++ youtube-batch_up.sh
@@ -6,16 +6,25 @@ echo "videos to upload:"
 var1=$1
 if [[ $var1 = "-h" || $var1 = "" ]]; then 
 	echo "pass args as follow in quotes \"*.*\" then playlist "
-	echo "ex: ./youtube-batch_up.sh \"1*\" \"ttc playlist\" "
+	echo "ex 1: ./youtube-batch_up.sh \"1*\" \"ttc playlist\" "
+	echo "ex 2: ./youtube-batch_up.sh \"1* 2*\" \"ttc playlist\" "
 	exit 0;
 fi
 
 numOfVidToUp=0
 for f in $var1; do
-	echo $f
-	((numOfVidToUp++))
+	if [[ -e f ]]; then
+		echo $f
+		((numOfVidToUp++))
+	fi
 done
-echo "will upload $numOfVidToUp videos."
+
+if [[ $numOfVidToUp -gt 0 ]]; then
+	echo "will upload $numOfVidToUp videos."
+else
+	echo "No files found mactching your filter!"
+	exit 1;
+fi
 
 read -p "do you want to upload them?y/n: " response
 if [[ ! $response = "y" ]]; then
