237c65929b166d3df556e76cca95aced80f17ae1
mohamed-elsaka <islamona2008@gmail.com>
2016-11-28 20:29:02 +0200

    add: support for multiple courses download
    add: support for multiple courses download
    $ psdown.sh username passoword course_url_1 course_url_2 
    OR by links in a file
    $ psdown.sh username passoword -f links_file.txt

--- psdown.sh
+++ psdown.sh
@@ -1,20 +1,29 @@
 #!/bin/bash 
 ps_username="$1"
 ps_password="$2"
-course_url="$3"
 
-var1=$1
-if [[ $var1 = "-h" || $var1 = "" ]]; then 
-        echo "pass username, password & course url as example:"
+first_course_url=$3
+if [[ $first_course_url = "-h" || $first_course_url = "" ]]; then 
+        echo "pass username, password & course urls as example:"
         echo "psdown.sh username password \"https://app.pluralsight.com/library/courses/django-angularjs-web-development\" "
         exit 0;
+
+elif [[ $first_course_url = "-f" ]]; then
+        ps_links_file=$4
+        while IFS= read -r course_url; do
+                command="youtube-dl --username $ps_username --password $ps_password --autonumber-size 2 $course_url -o './%(playlist)s/%(autonumber)s_%(title)s.%(ext)s'"
+                #echo $command
+                eval $command
+                echo "------------------Finished Downloading the course-------------------"
+
+        done < "$ps_links_file"
+
+else
+        for course_url in "${@:3}"; do
+                command="youtube-dl --username $ps_username --password $ps_password --autonumber-size 2 $course_url -o './%(playlist)s/%(autonumber)s_%(title)s.%(ext)s'"
+                #echo $command
+                eval $command
+                echo "------------------Finished Downloading the course-------------------"
+        done
 fi
-
-command="youtube-dl --username $ps_username --password $ps_password --autonumber-size 2 $course_url -o '%(autonumber)s_%(title)s.%(ext)$
-#echo $command
-eval $command
-echo "------------------Finished Downloading the course-------------------"
-
-
-
 
