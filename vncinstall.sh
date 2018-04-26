91af093009e4c8b13af12532a0ee99053976d94c
pythonoma <islamona2008@gmail.com>
2018-04-26 23:56:52 +0200

    Add 1GB Swap RAM by default

--- vncinstall.sh
+++ vncinstall.sh
@@ -7,3 +7,4 @@ eval "mv ~/.vnc/xstartup ~/.vnc/xstartup.bak";
 echo -e "#!/bin/bash \nxrdb $HOME/.Xresources \nstartxfce4 &" > ~/.vnc/xstartup ;
 eval "sudo chmod +x ~/.vnc/xstartup";
 eval "vncserver";
+eval "sudo fallocate -l 1G /mnt/1GB.swap && sudo mkswap /mnt/1GB.swap && sudo swapon /mnt/1GB.swap && sudo echo '/mnt/1GB.swap none swap sw 0 0' >> /etc/fstab && sudo swapon -s";
