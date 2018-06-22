118c4b2397d69ab78e1fa68a56d814379d867704
pythonoma <islamona2008@gmail.com>
2018-06-22 03:42:12 +0200

    Update youtube-batch_up.sh

--- youtube-batch_up.sh
+++ youtube-batch_up.sh
@@ -1,43 +1,47 @@
 #!/bin/bash 
-cred="leech1"
+cred="$HOME/.youtube-batch_up-creds"
+echo "$cred";
 playlist="$2"
 
 echo "videos to upload:"
 var1=$1
 if [[ $var1 = "-h" || $var1 = "" ]]; then 
-	echo "pass args as follow in quotes \"*.*\" then playlist "
-	echo "ex 1: ./youtube-batch_up.sh \"1*\" \"ttc playlist\" "
-	echo "ex 2: ./youtube-batch_up.sh \"1* 2*\" \"ttc playlist\" "
-	exit 0;
+        echo "pass args as follow in quotes \"*.*\" then playlist "
+        echo "ex 1: ./youtube-batch_up.sh \"1*\" \"ttc playlist\" "
+        echo "ex 2: ./youtube-batch_up.sh \"1* 2*\" \"ttc playlist\" "
+        exit 0;
 fi
 
 numOfVidToUp=0
 for f in $var1; do
-	if [[ -e $f ]]; then
-		echo $f
-		((numOfVidToUp++))
-	fi
+        if [[ -e $f ]]; then
+                echo $f
+                ((numOfVidToUp++))
+        fi
 done
 
 if [[ $numOfVidToUp -gt 0 ]]; then
-	echo "will upload $numOfVidToUp videos."
+        echo "will upload $numOfVidToUp videos."
 else
-	echo "No files found mactching your filter!"
-	exit 1;
+        echo "No files found mactching your filter!"
+        exit 1;
 fi
 
 read -p "do you want to upload them?[Y]/n: " response
 if [[ ! $response = "y" && ! $response = "" ]]; then
-	exit 1;
+        exit 1;
 fi
 
 numOfVidUploaded=0
 for f in $var1; do
-	#echo $f
-	command="youtube-upload --privacy=\"unlisted\" --playlist=\"$playlist\" --credentials-file=leech1  -t $f $f"
-	#echo $command
-	eval $command
-	((numOfVidUploaded++))
-	sleep 5
+        echo $f
+        f_esc=$(printf '%q' "$f");
+        echo $f_esc;
+        eval "touch \"$cred\"";
+        command="youtube-upload --privacy=\"unlisted\" --playlist=\"$playlist\" --credentials-file=\"$cred\"  -t \"$f\" $f_esc"
+        echo $command
+        eval $command
+        ((numOfVidUploaded++))
+        sleep 5
 done 
 echo "Uploaded $numOfVidUploaded/$numOfVidToUp"
